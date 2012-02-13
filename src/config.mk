78d67ea51bdf083f6270a558f58400b0aa9d8ee4
Scott Vokes <vokes.s@gmail.com>
2012-02-13 13:52:24 -0500

    Makefile cleanup

--- src/config.mk
+++ src/config.mk
@@ -1,27 +1,28 @@
 # Installation base path.
-PREFIX= /usr/local/
-MANPATH= ${PREFIX}share/man
+PREFIX=		/usr/local/
+MANPATH=	${PREFIX}share/man
 
 # If the OS is not recognized, you may need to uncomment one of these.
-#COPTS += -DHAS_GETLINE
-#COPTS += -DHAS_FGETLN
+#COPTS +=	-DHAS_GETLINE
+#COPTS +=	-DHAS_FGETLN
 
 # Where check in installed; only used for tests.
-CHECK_INC_PATH= ${PREFIX}include/
-CHECK_LIB_PATH= ${PREFIX}lib/
+CHECK_INC_PATH=	${PREFIX}include/
+CHECK_LIB_PATH=	${PREFIX}lib/
 
 #########################################################
 # Probably will not need to change anything after here. #
 #########################################################
 
 # Eliminate asserts (for performance).
-# DEBUG += -DNDEBUG
+# DEBUG +=	-DNDEBUG
 
 # Add profiling annotations.
-# PROFILE += -pg
+# PROFILE +=	-pg
 
 # Add debugging symbols.
-DEBUG += -g
+DEBUG +=	-g
 
-COPTS+= -O3 ${DEBUG}
-MANPATH1= ${MANPATH}/man1
+COPTS+=		-O2 ${DEBUG}
+COPTS+=		-Wall -pedantic
+MANPATH1=	${MANPATH}/man1
